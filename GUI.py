aa03bf6a1591ad54ce8a1208477bc1975008aa39
hendahan <hendahan110@gmail.com>
2020-12-14 08:35:22 +0200

    GUI
    The system will open a GUI menu where the user will be able to select the INPUT directory of the logs  in XML format.

---
+++ GUI.py
@@ -0,0 +1,85 @@
+import PySimpleGUI as sg
+
+from Logs_Extract import eventId_Extract
+
+dirIn_list = [
+    [
+        sg.Text("Enter input directory:  "),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-IN-"),
+        sg.FolderBrowse(),
+    ],
+
+]
+
+dirOut_list = [
+    [
+        sg.Text("Enter output directory:"),
+        sg.In(size=(25, 1), enable_events=True, key='-FOLDER-OUT-'),
+        sg.FolderBrowse(),
+    ],
+]
+
+butn_list = [
+    [
+        sg.Button("OK"),
+        sg.Exit()
+    ]
+]
+# check box layout
+checkBox_Malware_Archeology = [
+    [
+        sg.Checkbox('Malware Archeology')
+    ]
+]
+
+checkBox_miriam = [
+    [
+        sg.Checkbox('Miriam')
+    ]
+]
+
+checkBox_mitre_cti = [
+    [
+        sg.Checkbox('MITRE/cti')
+    ]
+]
+
+checkBox_list = [
+    [sg.Column(checkBox_miriam)],
+    [sg.Column(checkBox_mitre_cti)],
+    [sg.Column(checkBox_Malware_Archeology)]
+]
+
+
+
+
+
+
+
+
+layout = [
+
+        [sg.Column(dirIn_list)],
+        #sg.VSeperator(),
+        [sg.Column(dirOut_list)],
+        [sg.Column(checkBox_list)],
+        [sg.Column(butn_list)],
+
+
+
+
+]
+
+window = sg.Window("First Demo", layout)
+
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event in (None, 'Exit'):
+        break
+    if event == 'OK':
+        print("pressed OK")
+        eventId_Extract(values['-FOLDER-IN-'], values['-FOLDER-OUT-'])
+
+window.close()
