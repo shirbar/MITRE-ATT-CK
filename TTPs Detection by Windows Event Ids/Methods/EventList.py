df98402123f7bed87e162a1d9d3b3fe7f69dd5a4
kfir147 <kfir.rn@gmail.com>
2021-03-15 15:07:16 +0200

    Revert "Added Update for each Method."
    This reverts commit 2acd6a0c1dc68f16a11482d1fd752a3184a214c0.

--- TTPs Detection by Windows Event Ids/Methods/EventList.py
+++ TTPs Detection by Windows Event Ids/Methods/EventList.py
@@ -1,4 +1,3 @@
-import json
 import urllib.request
 import sqlite3
 import os.path
@@ -6,20 +5,15 @@ import os.path
 
 def update_event_list_db():
     url = "https://raw.githubusercontent.com/miriamxyra/EventList/master/EventList/internal/data/EventList.db"
-    fileName, headers = urllib.request.urlretrieve(url, 'Original Files/EventList.db')
-    with open("Util/MethodsDate.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-    data["EventList"] = headers['Content-Length']
-    with open("Util/MethodsDate.json", "w") as jsonFile:
-        json.dump(data, jsonFile)
+    urllib.request.urlretrieve(url, 'EventList.db')
 
 
 def get_event_list_hash_map():
     EventListHashMap = {}
-    if not os.path.exists("Original Files/EventList.db"):
+    if not os.path.exists("EventList.db"):
         update_event_list_db()
     try:
-        sqliteConnection = sqlite3.connect("Original Files/EventList.db")
+        sqliteConnection = sqlite3.connect("EventList.db")
         cursor = sqliteConnection.cursor()
         sqlite_select_Query = "select E.event_id, T.technique_id from mitre_events E, mitre_techniques T where " \
                               "E.technique_id = T.id; "
--- TTPs Detection by Windows Event Ids/Test/test_ExtraTests.py
+++
@@ -1,3 +0,0 @@
-from functools import reduce
-
-print((lambda x,y: lambda z: x+y if z==5 else pow(x,y))(1,2)(5))
